237772746d6d074d57006faea4f2f359423dd2df
GustavoPontess <gut.n.pontes@gmail.com>
2025-03-18 10:53:00 -0300

    refactor: remover arquivos antigos após refatoração para MVC

--- ControleDeCursos/Program.cs
+++ ControleDeCursos/Program.cs
@@ -16,7 +16,7 @@ namespace ControleDeCursos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPrincipal());
+            Application.Run(new MainView());
         }
     }
 }
